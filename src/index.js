11a161c80ea6ec5fb371ffa529ad03788bb2f347
Postman Integration <integration@postman.com>
2020-02-14 11:34:26 +0530

    Postman updated No Schema File Exists Draft

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-
-x
-xx
-adsf
+paths:
+  /users:
+    post:
+      summary: Adds a new user
+      requestBody:
+        content:
+          application/json:
+            schema:      # Request body contents
+              type: object
+              properties:
+                id:
+                  type: integer
+                name:
+                  type: string
+              example:   # Sample object
+                id: 10
+                name: Jessica Smith
+      responses:
+        '200':
+          description: OK
